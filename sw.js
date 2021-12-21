74f1250c2a491196254e3d8db7aa2b99a7f9a4fe
Ayub Metah <admin@ayubtechnologies.co.ke>
2021-12-21 23:51:19 +0300

    fix

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
 self.addEventListener('push', function(e) {
-    self.registration.sendNotification('test message', options);
-
+    var options = {
+        body: 'There are customers waiting in the Queue!',
+        icon: 'images/48.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [{
+                action: 'explore',
+                title: 'Open Workstation Terminal',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close',
+                title: 'Close',
+                icon: 'images/xmark.png'
+            },
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('Qmatic Orchestra', options)
+    );
 });
