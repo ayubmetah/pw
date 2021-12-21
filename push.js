75ac2c8d4248e2adcc8ff17d347e13853ef84b33
Ayub Metah <admin@ayubtechnologies.co.ke>
2021-12-21 22:53:48 +0300

    initial

---
+++ push.js
@@ -0,0 +1,15 @@
+var push = require('web-push');
+
+// var vapidKeys = push.generateVAPIDKeys();
+
+//console.log(vapidKeys)
+
+var vapidKeys = {
+    publicKey: 'BN82ji7NRhzohmiPwHL4P4TdEskv_eeUN7mchNO30VgWJfuU_bgphtPD1tEvJpljVwRI5KPIs6nGnftehzrdCPs',
+    privateKey: '7R9SRjYXP_nKsITnVw4Bjw2bYMPYdH0vgGV0VTzAs-I'
+};
+
+push.setVapidDetails('mailto:metta.job@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
+
+var sub = {};
+push.sendNotification(sub, 'test message');
